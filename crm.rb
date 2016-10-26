77b3b1d120891820eb1080d7bdcc2ceef6ffa1bd
terious78 <taesuk.moon@gmail.com>
2016-10-26 16:31:49 -0400

    Part 2: Layouts, HTML, & CSS

--- crm.rb
+++ crm.rb
@@ -5,9 +5,9 @@ require_relative 'contact'
 require 'sinatra'
 
 # Fake data
-Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
-Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
-Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+#Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+#Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+#Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
 
 get '/' do
   @crm_app_name = "Bitmaker's CRM"
